120485669f4e0c674472ea4fb9343774415cba44
onurcantimur <onurx86@gmail.com>
2019-05-04 10:57:25 +0300

    Header View bağlantısı yapıldı veri tabanı ve controller'daki hatalar giderildi

---
+++ estech/src/main/java/Models/Category.java
@@ -0,0 +1,22 @@
+package Models;
+
+public class Category {
+
+    String id,name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
